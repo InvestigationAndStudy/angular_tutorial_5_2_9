2b1518586ca07c5b34c2a26efefe9b1b29d68d3b
venya <mymyhighlight@gmail.com>
2018-04-23 03:26:17 +0300

    [main] added messages service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,21 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { AppRoutingModule } from './/app-routing.module';
 
 import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,    
-    HeroesComponent, HeroDetailComponent
+    HeroesComponent, HeroDetailComponent, MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule
   ],
-  providers: [HeroService],
+  providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
